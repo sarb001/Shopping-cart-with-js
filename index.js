d37c18ca8777a9ea155b34e5eaec9de87f04183c
SARB <mrsinghbusiness05@gmail.com>
2022-10-03 05:16:08 +0530

    Shopping cart Done

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let cart = document.querySelector('.cart');
 let closecart = document.querySelector('#close-cart');
 
 // open and close cart 
-carticon.onclick = () =>
-{
-    cart.classList.add("active");
-}
-closecart.onclick = () =>
-{
-    cart.classList.remove("active");
-}
+   carticon.onclick = () =>
+   {
+      cart.classList.add("active");
+   }
+   closecart.onclick = () =>
+   {
+      cart.classList.remove("active");
+   }
 
 // Cart working in JS => 
 
@@ -50,7 +50,6 @@ closecart.onclick = () =>
             var button = addcart[i];
             button.addEventListener('click',addcartclicked)
          }
-
      }
 
     //   function Remove items for cart (here select the parent or whole item)
@@ -61,7 +60,6 @@ closecart.onclick = () =>
         buttonclicked.parentElement.remove();
         updatetotal();
     }
-
 
     // Quantity Function 
 
@@ -107,10 +105,10 @@ closecart.onclick = () =>
         }
 
              var cartboxcontent = `
-               <img src = "img/product2.jpg"  alt = "" class = "cart-img" />    
+               <img src = "${prodimg}"  alt = "" class = "cart-img" />    
                 <div class = "detail-box"> 
-                        <div class = "cart-product-title">  Earbuds  </div>
-                       <div class = "cart-price"> $25 </div>
+                        <div class = "cart-product-title"> ${title}  </div>
+                       <div class = "cart-price"> ${price} </div>
                         <input type = "number" value = "1" class = "cart-quantity">
                 </div>
 
@@ -123,9 +121,6 @@ closecart.onclick = () =>
       cartshopbox.getElementsByClassName('cart-quantity')[0].addEventListener('change',quantitychanged);
 
       }
-
-
- 
 
      function updatetotal()
      {
